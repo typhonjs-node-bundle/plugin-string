c40fa4b8966b6c206026673d35a3ad66c1930265
typhonrt <support@typhonrt.org>
2021-01-05 20:25:51 -0800

    refactor - separate loader from init hook.

--- src/hooks/init/init.js
+++ src/hooks/init/init.js
@@ -1,45 +1,4 @@
-const { string }        = require('rollup-plugin-string');
-
-const { flags }         = require('@oclif/command');
-
-const { NonFatalError } = require('@typhonjs-node-bundle/oclif-commons');
-
-/**
- * Handles interfacing with the plugin manager adding event bindings to pass back a configured
- * instance of `rollup-plugin-string`.
- */
-class PluginHandler
-{
-   /**
-    * Returns the configured input plugin for `rollup-plugin-string`
-    *
-    * @param {object} bundleData - The CLI config
-    * @param {object} bundleData.cliFlags  - The CLI flags
-    *
-    * @returns {object} Rollup plugin
-    */
-   static getInputPlugin(bundleData = {})
-   {
-      if (bundleData.cliFlags && typeof bundleData.cliFlags.string === 'object')
-      {
-         return string(bundleData.cliFlags.string);
-      }
-   }
-
-   /**
-    * Wires up PluginHandler on the plugin eventbus.
-    *
-    * @param {PluginEvent} ev - The plugin event.
-    *
-    * @see https://www.npmjs.com/package/typhonjs-plugin-manager
-    *
-    * @ignore
-    */
-   static onPluginLoad(ev)
-   {
-      ev.eventbus.on('typhonjs:oclif:bundle:plugins:main:input:get', PluginHandler.getInputPlugin, PluginHandler);
-   }
-}
+const PluginLoader = require('../../loader/PluginLoader');
 
 /**
  * Oclif init hook to add PluginHandler to plugin manager.
@@ -52,10 +11,11 @@ module.exports = async function(opts)
 {
    try
    {
-      global.$$pluginManager.add({ name: '@typhonjs-node-rollup/plugin-string', instance: PluginHandler });
-
-      // Adds flags for various built in commands like `build`.
-      s_ADD_FLAGS(opts.id);
+      global.$$pluginManager.add({ name: '@typhonjs-node-rollup/plugin-string', instance: PluginLoader,
+         options: {
+            command: opts.id
+         }
+      });
 
       global.$$eventbus.trigger('log:debug', `plugin-string init hook running '${opts.id}'.`);
    }
@@ -64,74 +24,3 @@ module.exports = async function(opts)
       this.error(error);
    }
 };
-
-/**
- * Adds flags for various built in commands like `build`.
- *
- * Added flags include:
- * `--string`   - `-s` - Allows imports of string / text content.  - default: `(see below)` - env: DEPLOY_STRING'
- *
- * TODO: Test bad user entered data with the underlying Rollup plugin to see if a verification functionn needs to be
- * added here.
- *
- * @param {string} command - ID of the command being run.
- */
-function s_ADD_FLAGS(command)
-{
-   switch (command)
-   {
-      // Add all built in flags for the build command.
-      case 'bundle':
-         global.$$eventbus.trigger('typhonjs:oclif:system:flaghandler:add', {
-            command,
-            plugin: 'plugin-string',
-            flags: {
-               string: flags.string({
-                  'char': 's',
-                  'description': 'Allows imports of string / text content.',
-                  'multiple': true,
-                  'default': function()
-                  {
-                     if (typeof process.env.DEPLOY_STRING === 'string')
-                     {
-                        let result = void 0;
-
-                        // Treat it as a JSON array.
-                        try { result = JSON.parse(process.env.DEPLOY_STRING); }
-                        catch (error)
-                        {
-                           throw new NonFatalError(
-                            `Could not parse 'DEPLOY_STRING' as a JSON array;\n${error.message}`);
-                        }
-
-                        if (!Array.isArray(result))
-                        {
-                           throw new NonFatalError(`Please format 'DEPLOY_STRING' as a JSON array.`);
-                        }
-
-                        return result;
-                     }
-
-                     return ['**/*.html', '**/*.hbs'];
-                  }
-               })
-            },
-
-            /**
-             * Verifies the `string` flag and checks that the data loaded is an array and transforms the option
-             * into the proper format which is `{ includes: [...data] }`
-             *
-             * @param {object}   flags - The CLI flags to verify.
-             */
-            verify: function(flags)
-            {
-               // replace should always be an array
-               if (Array.isArray(flags.string))
-               {
-                  flags.string = { include: flags.string }
-               }
-            }
-         });
-         break;
-   }
-}
---
+++ src/loader/PluginLoader.js
@@ -0,0 +1,118 @@
+const { string }        = require('rollup-plugin-string');
+const { flags }         = require('@oclif/command');
+const { NonFatalError } = require('@typhonjs-node-bundle/oclif-commons');
+
+/**
+ * Handles interfacing with the plugin manager adding event bindings to pass back a configured
+ * instance of `rollup-plugin-string`.
+ */
+class PluginLoader
+{
+   /**
+    * Adds flags for various built in commands like `build`.
+    *
+    * Added flags include:
+    * `--string`   - `-s` - Allows imports of string / text content.  - default: `(see below)` - env: {prefix}_STRING'
+    *
+    * TODO: Test bad user entered data with the underlying Rollup plugin to see if a verification functionn needs to be
+    * added here.
+    *
+    * @param {string} command - ID of the command being run.
+    * @param {object} eventbus - The eventbus to add flags to.
+    */
+   static addFlags(command, eventbus)
+   {
+      switch (command)
+      {
+         // Add all built in flags for the build command.
+         case 'bundle':
+            eventbus.trigger('typhonjs:oclif:system:flaghandler:add', {
+               command,
+               plugin: 'plugin-string',
+               flags: {
+                  string: flags.string({
+                     'char': 's',
+                     'description': 'Allows imports of string / text content.',
+                     'multiple': true,
+                     'default': function()
+                     {
+                        const envVar = `${global.$$flag_env_prefix}_STRING`;
+
+                        if (typeof process.env[envVar] === 'string')
+                        {
+                           let result = void 0;
+
+                           // Treat it as a JSON array.
+                           try { result = JSON.parse(process.env[envVar]); }
+                           catch (error)
+                           {
+                              throw new NonFatalError(
+                                 `Could not parse '${envVar}' as a JSON array;\n${error.message}`);
+                           }
+
+                           if (!Array.isArray(result))
+                           {
+                              throw new NonFatalError(`Please format '${envVar}' as a JSON array.`);
+                           }
+
+                           return result;
+                        }
+
+                        return ['**/*.html'];
+                     }
+                  })
+               },
+
+               /**
+                * Verifies the `string` flag and checks that the data loaded is an array and transforms the option
+                * into the proper format which is `{ includes: [...data] }`
+                *
+                * @param {object}   flags - The CLI flags to verify.
+                */
+               verify: function(flags)
+               {
+                  // replace should always be an array
+                  if (Array.isArray(flags.string))
+                  {
+                     flags.string = { include: flags.string };
+                  }
+               }
+            });
+            break;
+      }
+   }
+
+   /**
+    * Returns the configured input plugin for `rollup-plugin-string`
+    *
+    * @param {object} bundleData - The CLI config
+    * @param {object} bundleData.cliFlags  - The CLI flags
+    *
+    * @returns {object} Rollup plugin
+    */
+   static getInputPlugin(bundleData = {})
+   {
+      if (bundleData.cliFlags && typeof bundleData.cliFlags.string === 'object')
+      {
+         return string(bundleData.cliFlags.string);
+      }
+   }
+
+   /**
+    * Wires up PluginHandler on the plugin eventbus.
+    *
+    * @param {PluginEvent} ev - The plugin event.
+    *
+    * @see https://www.npmjs.com/package/typhonjs-plugin-manager
+    *
+    * @ignore
+    */
+   static onPluginLoad(ev)
+   {
+      ev.eventbus.on('typhonjs:oclif:bundle:plugins:main:input:get', PluginLoader.getInputPlugin, PluginLoader);
+
+      PluginLoader.addFlags(ev.pluginOptions.command, ev.eventbus);
+   }
+}
+
+module.exports = PluginLoader;
